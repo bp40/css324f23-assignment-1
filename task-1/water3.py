1ff432fcbdb29dcaa9a1e541478f2a7d5f06b7e7
bp40 <60870525+bp40@users.noreply.github.com>
2023-09-10 15:49:02 +0700

    add sucessors implementation

--- task-1/water3.py
+++ task-1/water3.py
@@ -1,9 +1,21 @@
 def initial_state():
     return (8, 0, 0)
 
-def is_goal(s):
-    return s[0] == 4 and s[1] == 4
+def is_goal(state):
+    return state[0] == 4 and state[1] == 4
 
-def successors(s):
-    x, y, z = s
-    return []
+def successors(state):
+    capacities = [8,5,3]
+    # 8L=0, 5L=1, 3L=2
+    movePairs = [(0,1),(0,2),(1,0),(1,2),(2,0),(2,1)] 
+
+    for pair in movePairs:
+        source, destination = pair
+        # min(water in source , empty space in destination)
+        amount = min(state[source], capacities[destination] - state[destination])
+
+        if amount > 0:
+            updatedState = list(state)
+            updatedState[source] -= amount
+            updatedState[destination] += amount
+            yield (tuple(updatedState), amount)
